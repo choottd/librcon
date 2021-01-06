47a06c4bf13360c1ab8df4bad7245eac7a131d47
Giordano Battilana <giordano.battilana@novadart.com>
2021-01-06 01:19:30 +0100

    license updated

--- src/jvmMain/kotlin/org/choottd/librcon/packet/Packet.kt
+++ src/jvmMain/kotlin/org/choottd/librcon/packet/Packet.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020. Giordano Battilana
+ * Copyright 2021 Giordano Battilana
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
